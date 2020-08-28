b43cdce41553f3eb48bbbc31d3c0cfe9595a086b
Fernando Gonzalez <fallofrapture59@gmail.com>
2020-08-27 21:23:09 -0400

    Fix indentation, remove unused imports

--- src/main/java/me/jellysquid/mods/phosphor/common/EarlyRiser.java
+++ src/main/java/me/jellysquid/mods/phosphor/common/EarlyRiser.java
@@ -21,47 +21,47 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class EarlyRiser implements Runnable {
-        public static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
 
-        public void run() {
-                MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
+    public void run() {
+        MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
 
-                String lightStorage = remapper.mapClassName("intermediary", "net.minecraft.world.chunk.light.LightStorage");
-                ClassTinkerers.addTransformation(lightStorage, node -> {
-                        for (MethodNode mn : (List<MethodNode>) node.methods) {
-                                if ("updateLightArrays".equals(mn.name)) {
-                                        InsnList insns = mn.instructions;
-                                        Iterator<AbstractInsnNode> j = insns.iterator();
-                                        while (j.hasNext()) {
-                                                AbstractInsnNode i1 = j.next();
-                                                LOGGER.info(i1.getOpcode());
-                                                if (i1.getOpcode() == INVOKEINTERFACE) {
-                                                        AbstractInsnNode i2 = getNext(i1);
-                                                        if (i2 != null && i2.getOpcode() == CHECKCAST) {
-                                                                AbstractInsnNode i3 = getNext(i2);
-                                                                if (i3 != null && i3.getOpcode() == INVOKEVIRTUAL) {
-                                                                        InsnList il = new InsnList();
-                                                                        il.add(new MethodInsnNode(INVOKEINTERFACE, "it/unimi/dsi/fastutil/longs/LongIterator", "nextLong", "()J"));
-                                                                        insns.insert(i1.getPrevious(), il);
-                                                                        insns.remove(i1);
-                                                                        insns.remove(i2);
-                                                                        insns.remove(i3);
-                                                                }
-                                                        }
-                                                }
-                                        }
+        String lightStorage = remapper.mapClassName("intermediary", "net.minecraft.world.chunk.light.LightStorage");
+        ClassTinkerers.addTransformation(lightStorage, node -> {
+            for (MethodNode mn : (List<MethodNode>) node.methods) {
+                if ("updateLightArrays".equals(mn.name)) {
+                    InsnList insns = mn.instructions;
+                    Iterator<AbstractInsnNode> j = insns.iterator();
+                    while (j.hasNext()) {
+                        AbstractInsnNode i1 = j.next();
+                        LOGGER.info(i1.getOpcode());
+                        if (i1.getOpcode() == INVOKEINTERFACE) {
+                            AbstractInsnNode i2 = getNext(i1);
+                            if (i2 != null && i2.getOpcode() == CHECKCAST) {
+                                AbstractInsnNode i3 = getNext(i2);
+                                if (i3 != null && i3.getOpcode() == INVOKEVIRTUAL) {
+                                    InsnList il = new InsnList();
+                                    il.add(new MethodInsnNode(INVOKEINTERFACE, "it/unimi/dsi/fastutil/longs/LongIterator", "nextLong", "()J"));
+                                    insns.insert(i1.getPrevious(), il);
+                                    insns.remove(i1);
+                                    insns.remove(i2);
+                                    insns.remove(i3);
                                 }
+                            }
                         }
-                });
-        }
+                    }
+                }
+            }
+        });
+    }
 
-        private static AbstractInsnNode getNext(AbstractInsnNode insn) {
-                do {
-                        insn = insn.getNext();
-                        if (insn != null && !(insn instanceof LineNumberNode)) {
-                                break;
-                        }
-                } while (insn != null);
-                return insn;
-        }
+    private static AbstractInsnNode getNext(AbstractInsnNode insn) {
+        do {
+            insn = insn.getNext();
+            if (insn != null && !(insn instanceof LineNumberNode)) {
+                break;
+            }
+        } while (insn != null);
+        return insn;
+    }
 }
